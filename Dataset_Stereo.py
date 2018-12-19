39e9c19263cced7e714ccdc346d54f3757949ea0
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2018-12-19 14:17:59 -0800

    ...

--- Dataset_Stereo.py
+++ Dataset_Stereo.py
@@ -24,15 +24,24 @@ def get_device(device_id = 0):
 class Data_Moment():
     
     def __init__(self, images, speeds, start_index, n_frames, frame_gap, filename):
-        
+        '''
+        There can be no calculation with content of the hdf5 file or a selection of ranges here
+        because that will slow down loading and put a lot of data in memory
+        '''
         self.images = images
         self.speeds = speeds
         
         self.filename = filename
         
         self.start_index = start_index
-        self.stop_index = self.start_index + (n_frames*frame_gap)
+        self.stop_index = self.start_index + (n_frames*frame_gap) 
         self.frame_gap = frame_gap
+        
+        if self.stop_index >= len(images):
+            self.invalid = True
+        else:
+            self.invalid = False
+        
         
     def convert_images(self, encoded_images):
         return [cv2.imdecode(np.fromstring(encoded_img, dtype=np.uint8), -1) for encoded_img in encoded_images]      
@@ -40,7 +49,7 @@ class Data_Moment():
     def data_point(self):     
         
         indices = np.arange(self.start_index,self.stop_index,self.frame_gap)
-        print indices
+        
         # Speeds are one list, twice the size of images, because they are flattened out pairs of values.
         # They have to be reshaped to be pairs. It would be possible to select first the range on where
         # to do the reshape operation and then do it though it is assumed the operation takes about the
@@ -51,9 +60,8 @@ class Data_Moment():
         
         velocities = np.array(np.linalg.norm(speeds,axis=1),dtype=np.float32)
         course_list = np.array(BDD_Helper.to_course_list(speeds),dtype=np.float32)
-        #print course_list
-        #print np.diff(course_list)
-        #exit()
+        course_list = np.diff(course_list)
+        
         velocities_courses = np.array(zip(velocities,course_list))
         
         # Images have to be re-formatted into a numpy array because the special indexing does
@@ -107,8 +115,9 @@ class Dataset(data.Dataset):
                 
                 moment = Data_Moment(images, speeds, start_index, n_frames, frame_gap, filename)
                 
-                if moment is None:
+                if moment.invalid:
                     # At the end of a sequence no full scene can be compiled
+                    print "Moment too short"
                     break                
                 
                 self.run_files.append(moment)
@@ -123,7 +132,6 @@ class Dataset(data.Dataset):
         camera_data = torch.FloatTensor().to(get_device())
         
         for frame in range(self.n_frames): 
-            
             img = torch.FloatTensor(data_moment.data_point()['imgs'][frame]).to(get_device())
             camera_data = torch.cat((camera_data, img), 2)
              
@@ -133,10 +141,13 @@ class Dataset(data.Dataset):
 
         vel_course_pairs = torch.from_numpy(data_moment.data_point()['vel_course_pairs']).float().to(get_device()) 
         
+        # If there is no movement, course information become NAN. This is most likely because they calculate it 
+        # through a gyroscope which needs movement to tell the direction. We retrieve the change in course which
+        # will be 0 when there is no movement so we can catch NANs that way and replace them with zeros
         for i, value in enumerate(vel_course_pairs[:,1]):
             if np.isnan(value):
-                vel_course_pairs[i][1] = np.random.uniform(0,2*np.pi)
-        #vel_course_pairs[:,1][np.isnan(vel_course_pairs[:,1])] = np.random.uniform(0,2*np.pi) 
+                vel_course_pairs[i][1] = 0.
+         
         
         return camera_data, vel_course_pairs, index
     
@@ -152,10 +163,9 @@ if __name__ == '__main__':
     
     for i, (images, vel_course, index) in enumerate(train_loader):
         
-        for frame in np.split(images[0],6):
-            print frame.shape
-            img = frame.data.cpu().numpy()
-            img = img.transpose((1,2,0))+0.5
-            cv2.imshow("Test", img)
-            cv2.waitKey(90)
-    
+        img = images[0][6:9].data.cpu().numpy()
+        img = img.transpose((1,2,0))+0.5
+        print vel_course
+        cv2.imshow("Test", img)
+        cv2.waitKey(3)
+
