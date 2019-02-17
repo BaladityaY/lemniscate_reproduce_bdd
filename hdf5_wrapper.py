9ffd73dc90e1e64c7449280280c21131c13068f0
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2019-02-16 17:51:10 -0800

    ...

--- hdf5_wrapper.py
+++ hdf5_wrapper.py
@@ -1,22 +1,55 @@
 '''
 @author: Sascha Hornauer
 '''
+from collections import defaultdict
+
+class DB_file(object):
+    
+    def __init__(self):
+        pass
+        
+    def __getitem__(self,index):
+        print index
+        print "Getitem called"
 
 class DB_manager(object):
-    
-    
     
     def __init__(self, preload_to_mem, keep_memory_free):
         
         self.preload_to_mem = preload_to_mem
-        self.keep_memory_fre = keep_memory_free
+        self.keep_memory_free = keep_memory_free
+        
+        # The db manager holds three dictionaries for references to the data.
+        # Those references are either pointer to memory, file handles or path strings
+        # depending on which there is memory left, there are filehandles left or none
+        self.in_memory = defaultdict()
+        self.only_opened = defaultdict()
+        self.not_opened = defaultdict()
+        
+    def get_free_mem(self):
+        meminfo = dict((i.split()[0].rstrip(':'),int(i.split()[1])) for i in open('/proc/meminfo').readlines())
+        mem_available = float(meminfo['MemAvailable'])
+        mem_total = float(meminfo['MemTotal'])
+        return (mem_available/mem_total)*100.
 
-
+# def load_to_mem(hdf_reference):
+#     '''
+#     Loads the content of the hdf_reference dataset into memory, using the dataset reference as a key.
+#     This is only done if that reference does not yet exist in the buffer
+#     '''
+#     if not hdf_reference in memory_data_buffer: 
+#         memory_data_buffer.update({hdf_reference:hdf_reference[:]})
+#     # Return a pointer to the data in the dict
+#     return memory_data_buffer.get(hdf_reference)
 
     def add_file(self, filename):
         print "Add {}".format(filename)
-        pass
-    
-    def __getitem__(self,index):
-        print index
-        pass
+        
+        if self.preload_to_mem and self.get_free_mem() > self.keep_memory_free:
+            self.in_memory[filename] = DB_file(filename)
+        
+        
+        
+        return self
+
+        
