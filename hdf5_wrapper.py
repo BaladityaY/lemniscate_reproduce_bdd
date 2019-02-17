8413ef50f71f905475de5f5f87ecd2d235d350ed
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2019-02-17 12:17:03 -0800

    ...

--- hdf5_wrapper.py
+++ hdf5_wrapper.py
@@ -1,16 +1,24 @@
 '''
 @author: Sascha Hornauer
 '''
+from enum import Enum
+
+storage_type = Enum('storage_type','mem_handle file_handle string')
+
+class DB_item(object):
+    
+    def __init__(self, type):
+        pass
+
 
 class DB_manager(object):
-    
-    
     
     def __init__(self, preload_to_mem, keep_memory_free):
         
         self.preload_to_mem = preload_to_mem
         self.keep_memory_fre = keep_memory_free
-
+        
+        self.open_files = []
 
 
     def add_file(self, filename):
@@ -20,3 +28,7 @@ class DB_manager(object):
     def __getitem__(self,index):
         print index
         pass
+    
+if __name__ == '__main__':
+    print list(storage_type)
+    
