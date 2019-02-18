b477b6f1c12de39e459d72dd5f128c35ce1d4d0e
Sascha Hornauer <sascha.hornauer@uni-oldenburg.de>
2019-02-17 23:32:08 -0800

    ...

--- hdf5_wrapper.py
+++ hdf5_wrapper.py
@@ -3,52 +3,77 @@
 '''
 
 from enum import Enum
+import h5py
+from collections import deque
 
-storage_type = Enum('storage_type','mem_handle file_handle string')
+storage_type = Enum('storage_type', 'file_handle string')
 
 class DB_item(object):
     
-    def __init__(self, type):
-        pass
+    def __init__(self, filename, db_type, close_open_file, stack_open_file_method):
+        self.type = db_type
+        self.filename = filename
+        self.count_file_method = close_open_file
+        self.stack_open_file_method = stack_open_file_method
+        
+        if db_type == storage_type.file_handle:
+            self.handle = h5py.File(filename, 'r')
+        else:
+            self.handle = filename
+    
+    def __getitem__(self, index):
+        
+        if self.db_type == storage_type.file_handle:
+            return self.handle[index]
+        else:
+            self.close_open_file()
+            
+            new_open_file = h5py.File(self.filename, 'r')
+            self.handle = new_open_file
+            self.stack_open_file_method(new_open_file)
+            return new_open_file[index]
+        
+        
 
 class DB_manager(object):
     
-    def __init__(self, preload_to_mem, keep_memory_free):
+    def __init__(self, preload_to_mem, keep_memory_free, max_open_files=15000):
         
         self.preload_to_mem = preload_to_mem
         self.keep_memory_fre = keep_memory_free
         
-        self.open_files = []
-        
-    def get_free_mem(self):
-        meminfo = dict((i.split()[0].rstrip(':'),int(i.split()[1])) for i in open('/proc/meminfo').readlines())
-        mem_available = float(meminfo['MemAvailable'])
-        mem_total = float(meminfo['MemTotal'])
-        return (mem_available/mem_total)*100.
+        self.open_files = deque()
+        self.max_open_files = max_open_files
 
-
-# def load_to_mem(hdf_reference):
-#     '''
-#     Loads the content of the hdf_reference dataset into memory, using the dataset reference as a key.
-#     This is only done if that reference does not yet exist in the buffer
-#     '''
-#     if not hdf_reference in memory_data_buffer: 
-#         memory_data_buffer.update({hdf_reference:hdf_reference[:]})
-#     # Return a pointer to the data in the dict
-#     return memory_data_buffer.get(hdf_reference)
+    def close_open_file(self):
+        if len(self.open_files) >= self.max_open_files:
+            self.open_files[0].close()
+            self.open_files[0].pop(0)
+            
+    def stack_open_file(self, db_object):
+        self.open_files.append(db_object)
 
     def add_file(self, filename):
         print "Add {}".format(filename)
-
-        pass
-    
-    def __getitem__(self,index):
-        print index
         
+        if len(self.open_files) < self.max_open_files:
+            db_object = DB_item(filename, storage_type.file_handle)
+            self.stack_open_file(db_object)
+        else:
+            db_object = DB_item(filename, storage_type.string)
+        
+        return db_object
+    
+    
 #         if self.preload_to_mem and self.get_free_mem() > self.keep_memory_free:
 #             self.in_memory[filename] = DB_file(filename)
-        
-        return self
+# 
+#         
+#     def get_free_mem(self):
+#         meminfo = dict((i.split()[0].rstrip(':'),int(i.split()[1])) for i in open('/proc/meminfo').readlines())
+#         mem_available = float(meminfo['MemAvailable'])
+#         mem_total = float(meminfo['MemTotal'])
+#         return (mem_available/mem_total)*100.
 
 
     
