e0500a3ee2d15edf6af9733f31db09766a4239a5
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2019-03-06 19:03:38 -0800

    ...

--- models/resnet_late_fusion.py
+++ models/resnet_late_fusion.py
@@ -218,8 +218,7 @@ class ResNet(nn.Module):
         return all_flow_fields
 
     def forward(self, x, action_probabilities):
-        print x.size()
-        exit()
+        
         x = self.conv1(x)
         
         x = self.bn1(x)
